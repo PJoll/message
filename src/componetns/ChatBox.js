6896a39d132ad0183885a71aa6a7ee58705e3e9c
Peter Jolley <peter@marango.com.au>
2023-05-26 11:34:57 +0930

    chat box component complete

---
+++ src/componetns/ChatBox.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef, useState } from "react";
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import Message from "./Message";
+import SendMessage from "./SendMessage";
+
+const ChatBox = () => {
+  const [messages, setMessages] = useState([]);
+  const scroll = useRef();
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt"),
+      limit(100)
+    );
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let messages = [];
+      QuerySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe;
+  }, []);
+  return (
+    <main className="chat-box">
+      <div className="messages-wrapper">
+        {messages?.map((messages) => (
+          <Message key={messages.id} message={messages} />
+        ))}
+      </div>
+      <span ref={scroll}></span>
+      <SendMessage scroll={scroll} />
+    </main>
+  );
+};
+
+export default ChatBox;
