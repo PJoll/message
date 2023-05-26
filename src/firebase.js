0885dc486398f5850c8effc4b4fb525f75c34b5f
Peter Jolley <peter@marango.com.au>
2023-05-26 13:33:32 +0930

    firebase coded to include env config

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import dotenv from 'dotenv';
 
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+dotenv.config();
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBo2wLVvmX6j-E4Tm4VK91aHEz7xMP5-SY",
-  authDomain: "react-chat-c7a23.firebaseapp.com",
-  projectId: "react-chat-c7a23",
-  storageBucket: "react-chat-c7a23.appspot.com",
-  messagingSenderId: "548326395477",
-  appId: "1:548326395477:web:9e975dfb709f7fafe28edc",
-  measurementId: "G-4MEKHMN32H"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
